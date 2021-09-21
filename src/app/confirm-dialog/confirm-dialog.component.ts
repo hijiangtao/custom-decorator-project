186b02607c6fb0eb701f7d108fa8938617081ad8
hijiangtao <hijiangtao@gmail.com>
2021-09-21 17:19:29 +0800

    MOD: 完善弹窗组件交互

--- src/app/confirm-dialog/confirm-dialog.component.ts
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface DialogData {
+  title: '删除服务';
+}
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-dialog.component.css']
+  styleUrls: ['./confirm-dialog.component.css'],
 })
 export class ConfirmDialogComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
