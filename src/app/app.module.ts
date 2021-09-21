951b925747439d44c6e691f701123775fcd764be
hijiangtao <hijiangtao@gmail.com>
2021-09-21 17:11:44 +0800

    ADD: ConfirmDialogComponent 初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+import {
+  MatDialogModule,
+  MAT_DIALOG_DEFAULT_OPTIONS,
+} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+
+const THIRDPARTY_MODULES = [MatSliderModule, MatDialogModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, ConfirmDialogComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, ...THIRDPARTY_MODULES],
+  providers: [
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
